6178f8ae651d6b239533836791b779ba86a73176
Tayyeba <official.tayyeba@gmail.com>
2024-02-27 19:49:31 +0000

    getting to grips with turtle graphics in python

---
+++ heart.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen = turtle.getscreen()
+turtle = turtle.Turtle()
+
+turtle.right(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.backward(90)
+
+
+# Keep the Turtle graphics window open
+turtle.getscreen().mainloop()
