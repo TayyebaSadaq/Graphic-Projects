7327321c5d36df101f95d7804e114d0c4e7bb153
Tayyeba <official.tayyeba@gmail.com>
2024-02-27 20:29:54 +0000

    created heart - graphic finished

--- heart.py
+++ heart.py
@@ -2,19 +2,24 @@ import turtle
 
 screen = turtle.getscreen()
 t = turtle.Turtle()
+t.speed(5000)
+t.pensize(5)
 
-t.rt(-45)
-t.fd(100)
+for x in range(135):
+    t.rt(1)
+    t.fd(1)
+for x in range (135):
+    t.fd(1)
 t.rt(90)
-t.fd(100)
-t.rt(90)
-t.fd(200)
-t.rt(90)
-t.fd(200)
-t.rt(90)
-t.fd(100)
-t.rt(90)
-t.fd(100)
-
+for x in range (135):
+    t.fd(1)
+for x in range(200):
+    t.rt(1)
+    t.fd(1)
+t.lt(131)
+for x in range (70):
+    t.rt(1)
+    t.fd(1)
 # Keep the Turtle graphics window open
+
 turtle.getscreen().mainloop()
